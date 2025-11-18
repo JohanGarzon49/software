788bc3ebd9538e601db6f0af0d9f185e90f68a44
JohanGarzon49 <johangarzonqrt49@gmail.com>
2025-11-18 17:02:34 -0500

    Remove unused component imports from App.jsx
    Removed unused imports for Registro and OlvidoContrasena components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '/components/Login';
-import Registro from '/components/Registro';
-import OlvidoContrasena from '/components/OlvidoContrasena';
 function App() {
 return (
 <Router>
